6fb6e7a164690e2e41768fcc9e026c5959025b5b
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-06-10 19:21:59 -0600

    Show action descriptions on hover

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const actionDescriptions = {
+    wizard: [
+        "Releases a bolt of lightning that damages the enemy. Does 20 hp of damage and always hits.",
+        "Creates a deep fog that makes it harder for enemies to hit you.",
+        "A powerful attack that does 40 hp damage, but has an accuracy of 60%.",
+        "This wizard spent two decades perfecting this ancient dance move in the Shrine of Wisdom. It's unlikely to help, but you never know."
+        ]
+};
+
 const actionButtons = document.querySelectorAll(".hero-action");
 const dialog = document.querySelector(".dialog");
 const heroHPText = document.querySelector("#hero-hp");
 const enemyHPText = document.querySelector("#enemy-hp");
+const actionDescription = document.querySelector(".action-description");
 let enemyHP = 100;
 let heroHP = 100;
 
 actionButtons.forEach(btn => btn.addEventListener("click", activateAction));
+actionButtons.forEach(btn => btn.addEventListener("mouseover", showDescription));
 
 function activateAction(e) {
     console.log(e.target.value);
@@ -22,5 +33,11 @@ function enemyAction() {
     heroHPText.textContent = heroHP;
     setTimeout(() => {
         actionButtons.forEach(btn => btn.disabled = false);
+        dialog.textContent = "What will you do next?";
+        actionDescription.textContent = "Choose an action";
     }, 3000);
+}
+
+function showDescription(e) {
+    actionDescription.textContent = actionDescriptions.wizard[e.target.value - 1];
 }
